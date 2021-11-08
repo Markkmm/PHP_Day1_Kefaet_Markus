6859992a8b58b5c1aa7d47b31ca0709b800260ed
Romanimuss <88721472+Romanimuss@users.noreply.github.com>
2021-11-08 13:00:25 +0100

    Testing

--- PHP D1 Exercise Kefaet/Exer1Kef.php
+++ PHP D1 Exercise Kefaet/Exer1Kef.php
@@ -1,1 +1,11 @@
-<?php echo "Hello, World!"; ?>
+/*Exercise 1
+Print your name in an h1 HTML 
+element using PHP
+(define the variable
+and store your name into it).
+Use external CSS to position your
+name in the middle of the screen
+(horizontal, vertical).
+*/
+
+<?php echo "<h1 id=Name>Kefaet Prizreni !</h1>"; ?>
